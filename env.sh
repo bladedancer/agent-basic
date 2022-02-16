a79158e56fec4c02b39b5444bc2ce148866e4606
Gavin Matthews <gmatthews@axway.com>
2022-02-16 20:35:23 +0000

    Remove statefulset stuff.

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-script_dir=$(dirname ${BASH_SOURCE})
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 
 if [ "$ENV" = "preprod" ]; then
  . $script_dir/env-preprod.sh
